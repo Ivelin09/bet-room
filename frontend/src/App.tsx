fbcf9d3e28277e79a4752fa42006ae862728872c
justfrenzy22 <just.frenzy22@gmail.com>
2024-03-19 21:30:47 +0200

    fix merges

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,13 +2,10 @@
 import { Suspense, lazy } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import NavBar from './components/NavBar';
-<<<<<<< Updated upstream
-import Footer from './components/Footer';
-=======
-import TandC from './pages/t&c';
-import PrivacyAndCookiePolicy from './pages/PrivacyAndCookiePolicy';
+// import TandC from './pages/t&c';
+// import PrivacyAndCookiePolicy from './pages/PrivacyAndCookiePolicy';
 import { ContactForm } from './components/ContactForm';
->>>>>>> Stashed changes
+import Footer from './components/Footer';
 
 const Home = lazy(() => import('./pages/home'));
 const PrivacyAndCookiePolicy = lazy(() => import('./pages/PrivacyAndCookiePolicy'));
@@ -16,23 +13,6 @@ const TandC = lazy(() => import('./pages/t&c'));
 
 
 const App = () => {
-<<<<<<< Updated upstream
-  return (
-    <Router>
-      <>
-        <NavBar />
-        <Suspense fallback={<p>Loading...</p>} >
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path='/terms-and-conditions' element={<TandC />} />
-            <Route path='/privacy-and-cookie-policy' element={<PrivacyAndCookiePolicy />} />
-          </Routes>
-        </Suspense>
-        <Footer />
-      </>
-    </Router>
-  );
-=======
     return (
         <Router>
             <>
@@ -51,10 +31,10 @@ const App = () => {
                         />
                     </Routes>
                 </Suspense>
+                <Footer />
             </>
         </Router>
     );
->>>>>>> Stashed changes
 };
 
 export default App;
