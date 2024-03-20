130d6a2184d8f21170d34c916f2af9c44b6bbc75
justfrenzy22 <just.frenzy22@gmail.com>
2024-03-20 22:58:41 +0200

    navbar update

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -1,72 +1,96 @@
 import {
-    Button,
-    Link,
-    Navbar,
-    NavbarBrand,
-    NavbarContent,
-    NavbarItem,
-    NavbarMenu,
-    NavbarMenuItem,
-    NavbarMenuToggle,
+	Button,
+	Link,
+	Navbar,
+	NavbarBrand,
+	NavbarContent,
+	NavbarItem,
+	NavbarMenu,
+	NavbarMenuItem,
+	NavbarMenuToggle,
 } from '@nextui-org/react';
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 const NavBar = () => {
-    const menuItems = ['Features', 'Customers', 'Integrations'];
-    const [isMenuOpen, setIsMenuOpen] = useState(false);
-    const navigate = useNavigate();
+	const menuItems = [
+			{ label: 'Home', link: '/' },
+			{ label: 'About Us', link: '/about-us' },
+			{ label: 'Contact Us', link: '/contact-us' },
+			{ label: 'Betting Archive', link: '/archive' },
+	];
+	const [isMenuOpen, setIsMenuOpen] = useState(false);
+	const navigate = useNavigate();
 
-    return (
-        <Navbar isBordered position='static' onMenuOpenChange={setIsMenuOpen}>
-            <NavbarContent>
-                <NavbarMenuToggle
-                    aria-label={isMenuOpen ? 'Close menu' : 'Open menu'}
-                    className='sm:hidden'
-                />
-            </NavbarContent>
-            <NavbarBrand onClick={() => navigate('/')}>
-                <p className='font-bold cursor-pointer text-inherit'>BETROOM</p>
-            </NavbarBrand>
-            <NavbarContent className='hidden sm:flex gap-4' justify='end'>
-                <NavbarItem>
-                    <Link color='foreground' href='#'>
-                        Features
-                    </Link>
-                </NavbarItem>
-                <NavbarItem isActive>
-                    <Link href='#' aria-current='page'>
-                        Customers
-                    </Link>
-                </NavbarItem>
-                <NavbarItem>
-                    <Link color='foreground' href='#'>
-                        Integrations
-                    </Link>
-                </NavbarItem>
-            </NavbarContent>
-            <NavbarMenu>
-                {menuItems.map((item, index) => (
-                    <NavbarMenuItem key={`${item}-${index}`}>
-                        <Link
-                            color={
-                                index === 2
-                                    ? 'primary'
-                                    : index === menuItems.length - 1
-                                    ? 'danger'
-                                    : 'foreground'
-                            }
-                            className='w-full'
-                            href='#'
-                            size='lg'
-                        >
-                            {item}
-                        </Link>
-                    </NavbarMenuItem>
-                ))}
-            </NavbarMenu>
-        </Navbar>
-    );
+	return (
+		<Navbar
+			isMenuOpen={isMenuOpen}
+			isBordered
+			onMenuOpenChange={setIsMenuOpen}
+		>
+			<NavbarContent>
+				<NavbarMenuToggle
+					aria-label={isMenuOpen ? 'Close menu' : 'Open menu'}
+					className="sm:hidden"
+				/>
+				<NavbarBrand onClick={() => navigate('/')}>
+					<p className="font-bold cursor-pointer text-inherit">
+						BETROOM
+					</p>
+				</NavbarBrand>
+			</NavbarContent>
+			<NavbarContent
+				className="hidden sm:flex gap-4"
+				justify="end"
+			>
+				{
+                    menuItems.map((item: itemI, idx : number) => (
+                        <NavbarItem key={`${item}-${idx}`}>
+                            <Link
+                                className="cursor-pointer"
+                                size="lg"
+                                color="foreground"
+                                href={item.link}
+                            >
+                                {item.label}
+                            </Link>
+                        </NavbarItem>
+                    ))
+                }
+			</NavbarContent>
+			<NavbarMenu>
+				{menuItems.map((item: itemI, idx : number) => (
+					<NavbarMenuItem key={`${item}-${idx}`}>
+						<Link
+							className="w-full cursor-pointer"
+							size="lg"
+							color="foreground"
+                            href={item.link}
+						>
+							{item.label}
+						</Link>
+					</NavbarMenuItem>
+				))}
+				{/* {menuItems.map((item, index) => (
+					<NavbarMenuItem key={`${item}-${index}`}>
+						<Link
+							className="w-full"
+							
+							size="lg"
+                            color='foreground'
+						>
+							{item}
+						</Link>
+					</NavbarMenuItem>
+				))} */}
+			</NavbarMenu>
+		</Navbar>
+	);
 };
 
 export default NavBar;
+
+interface itemI {
+    label: string;
+    link: string;
+}
